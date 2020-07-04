a516d00cd24f2c6a2811ba19d176fa35f193dcd0
細川弘幸 <hiro@gitrhythm.local>
2020-07-05 00:25:41 +0900

    Add the top page

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="header container">
+    <h1 class="logo"><a href="/">Le café d'Hiro</a></h1>
+    <nav class="header-nav">
+      <ul>
+        <li><a href="menu">Menu</a></li>
+        <li><a href="news">News</a></li>
+        <li><a href="contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class PageHeader extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+  margin-bottom: 3rem;
+  padding-top: 2rem;
+  .logo {
+    font-family: "Life Savers", Open Sans;
+    font-size: 3.5rem;
+    font-weight: normal;
+    font-style: italic;
+    text-shadow: 1px 1px 3px #999;
+  }
+  ul {
+    font-size: 1.8rem;
+    display: flex;
+    text-transform: uppercase;
+    li {
+      margin-left: 1.6rem;
+    }
+  }
+}
+</style>
--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,13 +1,65 @@
 <template>
   <div>
-    home
+    <div id="top-visual" class="bg-image">
+      <page-header></page-header>
+
+      <main class="main container">
+        <h2 class="page-title">We'll Make Your Day</h2>
+        <p>
+          セーヌ左岸サン=ジェルマンに佇む当店のオープンテラス。<br>
+          ここからの街の息遣いをご堪能ください。<br>
+          ウジェーヌ・アジェが愛した古きパリの香りを、気の済むまで・・・
+        </p>
+        <div class="note">
+          <p>
+            注意)<br>
+            本サイトはサンプルであり、このカフェは実在しません。
+          </p>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import PageHeader from '../components/PageHeader.vue'
 
-@Component
-export default class Home extends Vue {
+@Component({
+  components: {
+    PageHeader
+  }
+})
+export default class Top extends Vue {
 }
 </script>
+
+<style scoped lang="scss">
+#top-visual {
+  min-height: 100vh;
+  background-image: url("../../public/images/top-bg.jpg");
+}
+
+.main {
+  text-align: right;
+  .page-title {
+    margin-top: 12%;
+    margin-bottom: 5rem;
+    text-transform: none;
+  }
+  p {
+    color: #000;
+  }
+  .note {
+    width: 50%;
+    margin: 0 4rem 0 auto;
+    margin-top: 6rem;
+    text-align: right;
+    p {
+      font-style: italic;
+      text-align: left;
+      color: red;
+    }
+  }
+}
+</style>
